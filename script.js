195b543fd3591d849106e8baeef74a7f649404c3
Lucas Rodrigues <Lucasrodrigues4@live.com>
2024-09-09 09:03:11 -0300

    modulando

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
 function toggleMode() {
+
   const html = document.documentElement;
-  /*  if (html.classList.contains("light")){
-html.classList.remove("light")
-  }else{
-    html.classList.add("light")
-  } 
-    */
+
   html.classList.toggle("light");
 }
+
+/*  if (html.classList.contains("light")){
+html.classList.remove("light")
+}else{
+  html.classList.add("light")
+} 
+  */
